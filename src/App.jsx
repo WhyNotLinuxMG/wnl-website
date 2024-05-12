f9b2c9aa6ab6a8a50516c10c4eaf551f77af7047
razafindratsiza <ainarazafindratsiza23@gmail.com>
2024-05-12 12:37:59 +0300

    creation of the home cover and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Home from './pages/Home'
+import './styles/App.css'
 function App() {
   
   return (
-    <>
+    <section className='container'>
       <Home/>
-    </>
+    </section>
   )
 }
 
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,8 +1,13 @@
+import Logo from '../ressources/logos_linux-tux.svg'
 const Nav = () => {
-    return <section className="navbar">
+    
+    return <header className="navbar">
         <div className="logo">
-            
+            <div className="wnl-logo">
+                <img src={Logo} height={70} width={70}/>
+            </div>
+            <div className="wnl-text-hidden-logo">Why not <span className='color-yellow-sea'>Linux</span></div>
         </div>
-    </section>
+    </header>
 }
 export default Nav
