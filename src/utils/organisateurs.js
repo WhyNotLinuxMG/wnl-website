149f35eb0010cf5e3d0c3da16af36f4980fb856d
Nj Nerson <nj.nerson@gmail.com>
2024-05-14 21:06:50 +0300

    added ressources organizers and partners

---
+++ src/utils/organisateurs.js
@@ -0,0 +1,15 @@
+import Esti from "../assets/images/ESTI.png";
+import Gdsc from "../assets/images/GDSC.png";
+import MadaLinux from "../assets/images/MLC.png";
+import It from "../assets/images/IT.png";
+
+export const organisateurs = [
+  { image: MadaLinux, nom: "MADA LINUX COMMUNITY" },
+  { image: Esti, nom: "ESTI" },
+  { image: Gdsc, nom: "GDSC" },
+  { image: It, nom: "iTeams" },
+  { image: MadaLinux, nom: "MADA LINUX COMMUNITY" },
+  { image: Esti, nom: "ESTI" },
+  { image: Gdsc, nom: "GDSC" },
+  { image: It, nom: "iTeams" },
+];
---
+++ src/utils/partenaires.js
@@ -0,0 +1,15 @@
+import Axian from "../assets/images/AXIAN.png";
+import Relia from "../assets/images/RELIA.png";
+import Etech from "../assets/images/ETECH.png";
+
+export const partenaires = [
+  { image: Axian, nom: "Groupe AXIAN" },
+  { image: Relia, nom: "RELIA Consulting" },
+  { image: Etech, nom: "ETECH" },
+  { image: Axian, nom: "Groupe AXIAN" },
+  { image: Relia, nom: "RELIA Consulting" },
+  { image: Etech, nom: "ETECH" },
+  { image: Axian, nom: "Groupe AXIAN" },
+  { image: Relia, nom: "RELIA Consulting" },
+  { image: Etech, nom: "ETECH" },
+];
