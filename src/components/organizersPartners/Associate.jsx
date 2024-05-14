87b777e05fe45d0de65347b9899e774f7d4c1ecf
Nj Nerson <nj.nerson@gmail.com>
2024-05-14 22:18:49 +0300

    resize logos

--- src/components/organizersPartners/Associate.jsx
+++ src/components/organizersPartners/Associate.jsx
@@ -1,11 +1,11 @@
 export default function Associate({ associate, ind }) {
   return (
-    <li key={ind} className="p-5 flex justify-center">
+    <li key={ind} className="p-5 w-[15rem]  h-[8rem] flex justify-center">
       <img
         draggable="false"
         src={associate.image}
         alt={associate.nom}
-        className=" md:w-full h-14 sm:h-16 md:h-20 object-cover block "
+        className=" w-full h-14 sm:h-16 md:h-20   object-contain block "
       />
     </li>
   );
