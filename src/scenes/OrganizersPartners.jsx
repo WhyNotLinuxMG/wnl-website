2e27b4c31d5a775d3e60dde073877e84e464f05f
Nj Nerson <nj.nerson@gmail.com>
2024-05-14 21:26:26 +0300

    structure f OrganizersPartners and titles

--- src/scenes/OrganizersPartners.jsx
+++ src/scenes/OrganizersPartners.jsx
@@ -1,9 +1,26 @@
+import { organisateurs, partenaires } from "../utils/";
+import Title from "../components/organizersPartners/Title";
+
+//ORGANISATEURS ET PARTENAIRES
 function OrganizersPartners() {
   return (
-    <div className="text-center py-4">
-        <h2 className="text-white text-lg">Organizers Partners</h2>
-    </div>
-  )
+    <section className=" bg-background py-[7.5rem] px-20 font-DMMono">
+      {/*ORGANISATEURS*/}
+      <div className="m-auto pb-20">
+        <Title>
+          Organisa<span className="text-yellow">teurs</span>{" "}
+        </Title>
+      </div>
+      {/* SEPARATORS */}
+      <div className=" m-auto h-3 box-border border-dashed border-y-2 mb-24 opacity-50 text-raven "></div>
+      {/*PARTENAIRES*/}
+      <div className="m-auto">
+        <Title>
+          Nos <span className="text-yellow">Partenaires</span>{" "}
+        </Title>
+      </div>
+    </section>
+  );
 }
 
-export default OrganizersPartners
+export default OrganizersPartners;
