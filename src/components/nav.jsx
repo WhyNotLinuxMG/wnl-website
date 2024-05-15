65b8c91c276a9ad25ecc81e0534e289351bb2798
razafindratsiza <ainarazafindratsiza23@gmail.com>
2024-05-15 13:31:00 +0300

    finish nav menu

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,4 +1,5 @@
 import Logo from '../ressources/logos_linux-tux.svg'
+import Drop from './Drop'
 import NavItem from './NavItem'
 
 const Nav = () => {
@@ -14,6 +15,11 @@ const Nav = () => {
             </div>
             <div className='flex items-center space-x-4 leading-5 sm:space-x-6'>
                 <NavItem>À propos</NavItem>
+                <Drop/>
+                <NavItem>Archive</NavItem>
+                <NavItem>FaQ</NavItem>
+                <button type="button" class="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2">inscription</button>
+
             </div>
         </header>
 }
