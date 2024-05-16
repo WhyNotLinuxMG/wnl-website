2e8bb458dfe848531a0bfaf218c43bc4b6ba599d
razafindratsiza <34934305+razafindratsiza@users.noreply.github.com>
2024-05-17 01:03:52 +0300

    Section home cover (#22)
    * finish nav menu
    
    * ajust nav margin
    
    * ajust nav-menu margin
    
    * ajust logo margin ans change style to taiwind
    
    * create cover section

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -1,5 +1,5 @@
 import Nav from "../components/nav";
-
+import ButtonFill from "../components/ButtonFill";
 const Home = () => {
   return (
     <div className="top-section">
@@ -7,9 +7,16 @@ const Home = () => {
         <div className='max-w-[1700px] mx-auto pt-5'>
           <div className="flex flex-col justify-between">
             <Nav />
-            <div className="text-center my-4">
-              <h1 className="text-white">HOME</h1>
+            <div className="mx-auto max-w-7xl ">
+              <div className='pt-6 mt-6 '>
+                <ButtonFill content="22 Juin 2024" />
+                <div className="backtitle">
+                  <h2 className='Kontes-Compressed-Bold text-[38px] md:text-[78px] text-white  pl-3 md:pl-0'>Empowering Communities</h2>
+                  <h2 className='Kontes-Compressed-Bold text-[38px] md:text-[78px] text-white pl-3 md:pl-0'>Through <span className="color-raven">Open Source</span></h2>
+                </div>
+              </div>
             </div>
+
           </div>
         </div>
       </div>
