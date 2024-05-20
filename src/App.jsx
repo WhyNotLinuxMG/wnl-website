b59c5c840e91fc5a2b415a3f767b9d17e1c8303f
Ntsoa2112 <manambintsoa.mihaingoherilanto@esti.mg>
2024-05-20 22:05:17 +0300

    Update Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
+import Comptetion from './page/Comptetion'
 import Conferences from './page/Conferences'
 import Stande from './page/Stande'
-import Comptetion from './page/Comptetion'
 import Statistiques from './page/Statistiques'
 import Home from './scenes/Home'
 import './styles/App.css'
 
-import OrganizersPartners from './scenes/OrganizersPartners'
-import Stat from './scenes/Stat'
+import About from './scenes/About'
 import Challenges from './scenes/Challenges'
+import Footer from './scenes/Footer'
+import GetTicket from './scenes/GetTicket'
+import OrganizersPartners from './scenes/OrganizersPartners'
 import Photos from './scenes/Photos'
 import Questions from './scenes/Questions'
-import GetTicket from './scenes/GetTicket'
-import Footer from './scenes/Footer'
-import About from './scenes/About'
+import Stat from './scenes/Stat'
 
 const App = () => {
   return (
     <>
       <Home />
       <div className="bg-background">
-
-
         <div className="mx-auto max-w-[1600px]  ">
           <OrganizersPartners/>
-           <About/>
+          <About/>
           <Conferences/>
           <Stande/>
           <Statistiques/>
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -18,7 +18,7 @@ const Nav = () => {
                 <Drop/>
                 <NavItem>Archive</NavItem>
                 <NavItem>FaQ</NavItem>
-                <button type="button" class="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2">inscription</button>
+                <button type="button" className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2">inscription</button>
 
             </div>
         </header>
