9d153c590581ab41b5b6594a554405cf63b3f170
EmilieNirinasoa <fanomezanaemilienirinasoa@gmail.com>
2024-05-21 22:45:33 +0300

    foires aux questions (#3)

--- src/scenes/Questions.jsx
+++ src/scenes/Questions.jsx
@@ -5,10 +5,10 @@ function Questions() {
     <div className="text-center my-4">
         <h2 className=" font-kontes text-[38px] md:text-[70px] text-white text-white pb-[5rem] p-5 md:pl-0 text-lg">Foires aux <span className="text-yellow">questions</span></h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4 ">
-        <div className="flex justify-center p-4 md:col-span-2">
+        <div className="flex justify-center p-4 md:col-span-1">
           <img src={Pingouin} alt="" />
         </div>
-        <div className="p-4  md:col-span-1 text-left">
+        <div className="p-4  md:col-span-2 text-left">
           <div className="question font-bold flex items-center justify-between border-b border-white pb-4 mb-4">
             À qui s'adresse cet événement?
             <IoIosArrowDown className="text-2xl text-white" />
