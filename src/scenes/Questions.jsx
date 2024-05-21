053c5fe361f647006f6d9d2300d74e9a23a82fdc
EmilieNirinasoa <fanomezanaemilienirinasoa@gmail.com>
2024-05-21 21:52:40 +0300

    foires aux questions

--- src/scenes/Questions.jsx
+++ src/scenes/Questions.jsx
@@ -1,7 +1,8 @@
 function Questions() {
   return (
     <div className="text-center my-4">
-        <h2 className="text-white text-lg">Foires aux questions</h2>
+        <h2 className=" font-kontes text-[38px] md:text-[58px] text-white text-white pb-[5rem] pl-3 md:pl-0 text-lg">Foires aux <span className="text-yellow">questions</span></h2>
+        
     </div>
   )
 }
