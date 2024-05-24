b6af1a5a46dc851b2e75f0982539d3c30bab6b20
EmilieNirinasoa <fanomezanaemilienirinasoa@gmail.com>
2024-05-24 06:03:56 +0300

    foires aux questions (#6)

--- src/scenes/Questions.jsx
+++ src/scenes/Questions.jsx
@@ -1,4 +1,4 @@
-import Pingouin from '../assets/images/pin.png';
+import Pingouin from '../assets/images/pin2.png';
 import { questions_et_reponses } from '../utils/questions_et_reponses';
 import Question from '../components/Question';
 
@@ -8,11 +8,11 @@ function Questions() {
       <h2 className="font-kontes text-[38px] md:text-[70px] text-white pb-[5rem] p-5 md:pl-0 text-lg">
         Foire aux <span className="text-yellow">questions</span>
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
+      <div className="grid grid-cols-2 md:grid-cols-2 ">
         <div className="flex justify-center p-4 md:col-span-1">
           <img src={Pingouin} alt="" />
         </div>
-        <div className="p-4 md:col-span-2 text-left">
+        <div className="p-4  text-left h-auto min-h-[200px] max-h-[800px] overflow-y-auto">
           {questions_et_reponses.map((item, index) => (
             <Question 
               item={item} 
