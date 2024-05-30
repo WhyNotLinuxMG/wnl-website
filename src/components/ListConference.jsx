033a54855091405cd9ed062f1438ffef8e343aac
Ntsoa2112 <manambintsoa.mihaingoherilanto@esti.mg>
2024-05-30 23:15:31 +0300

    chore: Update Conference component layout and styles

--- src/components/ListConference.jsx
+++ src/components/ListConference.jsx
@@ -8,8 +8,10 @@ const ListConference = ({image,content,hours}) => {
         transition={{duration:.6, ease:'easeIn'}}
         viewport={{ once: true }}
         className='relative m-5'>
-      <img className='' src={image}  alt="" />
-      <div className="absolute inset-0 bg-gradient-to-t from-black opacity-50 w-full"></div>
+      <div className="relative rounded-3xl overflow-hidden w-full">
+        <img className="w-full" src={image} alt="Example Image" />
+        <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-85"></div>
+      </div>
       <div className='text-white absolute bottom-3 left-9   '>
         <h4 className='font-DMMono text-[22px] font-semibold pb-5'>{content}</h4>
         <p className='font-medium text-raven'>{hours}</p>
