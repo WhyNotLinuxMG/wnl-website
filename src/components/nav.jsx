5bca9dc477bf97fa977ae381e35623f9282f82df
razafindratsiza <ainarazafindratsiza23@gmail.com>
2024-05-30 11:39:45 +0300

    adjust container size

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -25,19 +25,27 @@ const Nav = () => {
                 <div className="mr-3">
                     <img src={Logo} />
 
-                    </div>
-                    <div className="hidden h-6 text-3xl text-white text-center  Kontes-Compressed sm:block">Why not <span className='color-yellow-sea'>Linux</span></div>
                 </div>
+                <div className="hidden h-6 text-3xl text-white text-center  Kontes-Compressed sm:block">Why not <span className='color-yellow-sea'>Linux</span></div>
             </div>
+        </div>
 
-            <div className='flex items-center space-x-6 leading-5 sm:space-x-10'>
-                <NavItem>À propos</NavItem>
-                <Drop/>
-                <NavItem>Archive</NavItem>
-                <NavItem>FaQ</NavItem>
-                <button type="button" className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2">inscription</button>
-                </div>
-        
+        <div className='flex items-center space-x-6 leading-5 sm:space-x-10'>
+            <NavItem>À propos</NavItem>
+            <Drop />
+            <NavItem>Archive</NavItem>
+            <NavItem>FaQ</NavItem>
+            <button className="lg:hidden flex flex-col ml-4">
+                    <span className="w-6 h-1 bg-white mb-1">
+                    </span>
+                    <span className="w-6 h-1 bg-white mb-1">
+                    </span>
+                    <span className="w-6 h-1 bg-white mb-1">
+                    </span>
+                </button>
+            <button type="button" className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2">inscription</button>
+        </div>
+
     </header>
 }
 export default Nav
--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -1,26 +1,12 @@
 import Nav from "../components/nav";
 import ButtonFill from "../components/ButtonFill";
+import Hero from "../page/Hero";
 const Home = () => {
   return (
-    <div className="top-section">
-      <div className="mx-6">
-        <div className='max-w-[1700px] mx-auto pt-5'>
-          <div className="flex flex-col justify-between">
-            <Nav />
-            <div className="mx-auto max-w-7xl ">
-              <div className='pt-6 mt-6 '>
-                <ButtonFill content="22 Juin 2024" />
-                <div className="backtitle">
-                  <h2 className='Kontes-Compressed-Bold text-[38px] md:text-[78px] text-white  pl-3 md:pl-0'>Empowering Communities</h2>
-                  <h2 className='Kontes-Compressed-Bold text-[38px] md:text-[78px] text-white pl-3 md:pl-0'>Through <span className="color-raven">Open Source</span></h2>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
+    <main className="top-section dark:bg-gray-800 bg-white relative overflow-hidden h-screen">
+      <Nav />
+      <Hero />
+    </main>
   );
 }
 
