d6bc567c5f8825699e76751a7e57a45d13ad5e84
Nj Nerson <nj.nerson@gmail.com>
2024-05-30 19:55:33 +0300

    fix double same  import in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Comptetion from './page/Comptetion'
-import Conferences from './page/Conferences'
-import Stande from './page/Stande'
-import Statistiques from './page/Statistiques'
-import Home from './scenes/Home'
-import './styles/App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Comptetion from "./page/Comptetion";
+import Conferences from "./page/Conferences";
+import Stande from "./page/Stande";
+import Statistiques from "./page/Statistiques";
+import Home from "./scenes/Home";
+import "./styles/App.css";
 
-import CountDownDay from './scenes/CountDownDay'
-import OrganizersPartners from './scenes/OrganizersPartners'
-import Stat from './scenes/Stat'
+import CountDownDay from "./scenes/CountDownDay";
+import OrganizersPartners from "./scenes/OrganizersPartners";
+import Stat from "./scenes/Stat";
 
-import Challenges from './scenes/Challenges'
-import CountDownDay from './scenes/CountDownDay'
-import Footer from './scenes/Footer'
-import GetTicket from './scenes/GetTicket'
-import Photos from './scenes/Photos'
-import Questions from './scenes/Questions'
-import About from './scenes/About'
+import Challenges from "./scenes/Challenges";
+import Footer from "./scenes/Footer";
+import GetTicket from "./scenes/GetTicket";
+import Photos from "./scenes/Photos";
+import Questions from "./scenes/Questions";
+import About from "./scenes/About";
 
 const App = () => {
   const WNL = new Date("June 22, 2024").getTime();
@@ -25,24 +24,23 @@ const App = () => {
       <Home />
       <div className="bg-background">
         <div className="mx-auto max-w-[1600px]  ">
-          <CountDownDay WNL={WNL}/>
-          <OrganizersPartners/>
-          <About/>
-          <Conferences/>
-          <Stande/>
-          <Statistiques/>
-          <Comptetion/>
-          <Stat/>
-          <Challenges/>
-          <Photos/>
-          <Questions/>
-          <GetTicket/>
-          <Footer/>
-        </div>     
-
+          <CountDownDay WNL={WNL} />
+          <OrganizersPartners />
+          <About />
+          <Conferences />
+          <Stande />
+          <Statistiques />
+          <Comptetion />
+          <Stat />
+          <Challenges />
+          <Photos />
+          <Questions />
+          <GetTicket />
+          <Footer />
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
