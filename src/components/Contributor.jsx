977d1699ad0e126a750723b8e9ccd7670ed56959
dorissylvie <vololonirinadorissylvie@gmail.com>
2024-06-01 18:49:16 +0300

    liste contributeur

--- src/components/Contributor.jsx
+++ src/components/Contributor.jsx
@@ -7,25 +7,17 @@ function Contributor() {
         arrows: false,
         dots: false,
         infinite: true,
-        slidesToShow: 5,
         slidesToScroll: 1,
         autoplay: true,
+        variableWidth: true,
         speed: 3000,
         autoplaySpeed: 1000,
         cssEase: "linear",
         pauseOnHover: false,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-
-                }
-            }
-        ]
     };
     return (
         <>
-            <div className='slider-container pb-20'>
+            <div className='slider-container pb-20 w-[95%] m-auto'>
                 <Slider {...settings}>
                     {
                         contributors.map((contributor, index) => (
@@ -33,9 +25,7 @@ function Contributor() {
                         ))
                     }
                 </Slider>
-
             </div>
-
         </>
 
     )
@@ -46,19 +36,12 @@ export default Contributor
 function Contributor_card({ nom, fonction, image }) {
     return (
         <>
-            {/* <div className='relative mx-3'>
-                <img src={image} alt="" className=' rounded-3xl top-0 left-0' />
-                <div className=' bottom-3 left-2 absolute bg-red-500 w-full '>
+            <div className='contrib-container relative mx-3 h-[424px] w-[282px] '>
+                <img src={image} className="w-full h-full object-cover   " />
+                <div className='w-full h-1/4 bg-gradient-to-b from-[#25252506] via-[#000000d1] to-black  absolute bottom-0 p-4'>
                     <div className=' font-DMMono text-slate-200 text-lg'>{nom}</div>
-                    <div className='font-DMMono text-[#3f3f3fd4] text-lg'>{fonction}</div>
+                    <div className='font-DMMono text-[#8f8f8fd4] text-lg'>{fonction}</div>
                 </div>
-            </div > */}
-
-            <div className='contrib-container relative mx-3'>
-
-                <img src={image} className="h-[424px] w-[282px]" />
-
-
                 <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
                     <defs>
                         <filter id="flt_tag">
@@ -68,10 +51,7 @@ function Contributor_card({ nom, fonction, image }) {
                         </filter>
                     </defs>
                 </svg>
-
             </div>
-            {/* <div className='contrib-container  relative mx-3'>
-            </div> */}
         </>
     )
 }
--- src/utils/contributors.js
+++ src/utils/contributors.js
@@ -1,11 +1,10 @@
-import Conf1 from "../assets/images/nini.jpeg";
+import Conf1 from "../assets/images/conf1.png";
 import Conf2 from "../assets/images/conf2.png";
 import Conf3 from "../assets/images/conf3.png";
 import Conf4 from "../assets/images/conf4.png";
 import Conf5 from "../assets/images/conf5.png";
 import Conf6 from "../assets/images/conf6.png";
 import Conf7 from "../assets/images/conf7.png";
-import Conf8 from "../assets/images/conf8.png";
 
 export const contributors = [
   {
