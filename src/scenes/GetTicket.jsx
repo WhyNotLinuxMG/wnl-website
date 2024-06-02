2d5a25480a30e8fbc9556232e744f7f6b9021855
Nj Nerson <nj.nerson@gmail.com>
2024-06-02 22:27:33 +0300

    readjust get ticket section

--- src/scenes/GetTicket.jsx
+++ src/scenes/GetTicket.jsx
@@ -1,21 +1,22 @@
-import Binary from "../assets/images/binary.png"
-import ButtonFill from "../components/ButtonFill"
-import GetTicketButton from "../components/GetTicketButton"
+import Binary from "../assets/images/binary.png";
+import ButtonFill from "../components/ButtonFill";
+import GetTicketButton from "../components/GetTicketButton";
 function GetTicket() {
   return (
-    <div className="flex flex-col items-center text-center px-14 py-12 md:py-20  gradientBg relative">
+    <div className="flex flex-col justify-center items-center gap-y-5 h-screen text-center px-14 py-0 md:py-20  gradientBg relative">
       <ButtonFill content="22 Juin 2024"></ButtonFill>
       <h2 className="text-[38px] sm:text-[56px] md:text-[98px] text-white font-kontes">
         Ne ratez pas cet <span className="text-raven">évènement</span>
-      <br />incontournable !
+        <br />
+        incontournable !
       </h2>
-      <GetTicketButton/>
-      <div className="pb-5 absolute bottom-[-2rem] ">
-        <img src={Binary} className="h-[100px]" alt="" />
-        
+      <GetTicketButton />
+      <div className=" absolute left-0 bottom-0">
+        <img src={Binary} className="h-[70px]" alt="" />
+        <img src={Binary} className="h-[70px] lg:hidden " alt="" />
       </div>
     </div>
-  )
+  );
 }
 
-export default GetTicket
+export default GetTicket;
