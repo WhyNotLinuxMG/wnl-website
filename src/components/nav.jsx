111686029f2381b38c05ecb2f4c5107a06eb7f34
razafindratsiza <ainarazafindratsiza23@gmail.com>
2024-06-03 20:34:37 +0300

    changing singin button font and text

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -116,9 +116,9 @@ const Nav = () => {
 
         <button
           type="button"
-          className="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2"
+          className="text-white bg-[#050708] font-DMMono hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 me-2 mb-2"
         >
-          inscription
+          Inscription
         </button>
       </div>
     </header>
