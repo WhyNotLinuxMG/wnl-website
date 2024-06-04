4baf0ce804e77be48891416d6a487b0bd91da7ba
rivoalpha <rrajaonarivony.ext@baobabgroup.com>
2024-06-04 09:59:18 +0300

    [FIX] Photo Emitech

--- src/utils/organizers.js
+++ src/utils/organizers.js
@@ -1,6 +1,6 @@
 import MadaLinux from "../assets/images/madalinux.png";
 import It from "../assets/images/logoiTeams.png";
-import Emitech from "../assets/images/logoEmitechLight.jpeg";
+import Emitech from "../assets/images/logoEmitechLight.png";
 import C3lf from "../assets/images/logoC3lfDark.jpeg";
 
 export const organizers = [
