0082e795f395af698c730c1d486ad887648665e7
rivoalpha <rivo2302@gmail.com>
2024-07-16 10:10:36 +0300

    [FIX] content

--- src/components/SponsorPopup.jsx
+++ src/components/SponsorPopup.jsx
@@ -5,7 +5,7 @@ const SponsorPopup = ({ isOpen, onClose }) => {
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-gray-800 text-whiteSmoke p-6 rounded shadow-lg w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
         <p className="my-4">Vous êtes intéressé à devenir sponsor de l'événement Why Not Linux pour la prochaine édition ?</p>
-        <p className="my-4">Rejoignez-nous en envoyant un message à notre page Facebook (liens vers notre page facebook) ou à l'email suivant : rivo2302@gmail.com.</p>
+        <p className="my-4">Rejoignez-nous en envoyant un message à <a href="https://web.facebook.com/profile.php?id=100091340490173">  notre page Facebook </a> ou à l'email suivant : rivo2302@gmail.com.</p>
         <p className="my-4">Nous espérons vous compter parmi nous pour cet événement dédié à Linux et à l'open source.</p>
         <button
           onClick={onClose}
